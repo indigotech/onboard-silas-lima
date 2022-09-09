2334f5adf0c04991e71e4927e2d535f17c36a1f5
Taqtile <taqtile@Ariel.local>
2022-09-09 16:15:10 -0300

    adding eol

--- services/apollo.ts
+++ services/apollo.ts
@@ -3,4 +3,4 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
     uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
     cache: new InMemoryCache()
-  });
+});
